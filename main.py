9b9cc88644c73d754fcdf17c10a404fd45e88788
Patryk Janowski <pkjanowski@gmail.com>
2023-10-11 15:28:12 +0200

    _

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from hashlib import sha256
 import json
 import os, struct
 import socket
+import JSONpath
+
 
 class Block:
     def __init__(self, index, previous_hash, data, owner, nonce=0):
@@ -42,15 +44,23 @@ class Blockchain():
         self.owners_ip = dict()
 
     def join_blockchain(self, blockchain_address):
-        # return {"Chain": chain, "UserIps": set(users_ips)}
+        self.send_ips()
         self.listen_for_ips()
         self.listen_for_blocks()
         pass
 
     def publish_block(self, block: Block, block_hash):
         #port 50000
-        tmp_blockchain = new
+        tmp_blockchain = self.chain.copy()
+        tmp_blockchain.append({"Block": block, "BlockHash": block_hash})
+        # send tmp block
+        # wait for blocks
+        if True:
+            self.chain = tmp_blockchain
         return True
+    
+    def send_blockchain(self):
+        pass
 
     def listen_for_blocks(self):
         #port 50000
@@ -61,10 +71,16 @@ class Blockchain():
         pass
 
     def vote_for_block(self):
+        #port 50001
+        pass
+
+    def send_ips(self):
+        #port 50002
         pass
 
     def listen_for_ips(self):
         #port 50002
+        self.send_blockchain()
         pass
 
     def get_my_ip(self):
@@ -91,26 +107,23 @@ class Blockchain():
         new_block_hash = new_block.mine_block()
         self.chain.append({"Block": new_block, "BlockHash": new_block_hash})
         self.owners_ip[self.my_id] = self.my_ip 
-        self.publish_block(block=new_block, block_hash=new_block_hash)
         
-    def add_block(self, data):
+    def create_block(self, data):
         new_block = Block(owner=self.my_id, 
                           data=data, 
                           index=len(self.chain),
                           previous_hash=self.chain[-1]["BlockHash"])
         new_block_hash = new_block.mine_block()
-        if self.publish_block(new_block, new_block_hash):
-            self.chain.append({"Block": new_block, "BlockHash": new_block_hash})
-
-    def validate_blockchain(self, block: Block, block_hash):
-        return block.hash == block_hash
-
-
+        self.publish_block(new_block, new_block_hash)
+        
             
+    def validate_blockchain(self, block: Block, block_hash):
+        return block.hash == block_hash and self.chain
+ 
     
 bc = Blockchain(1)
 bc.create_blockchain("xd")
-bc.add_block("hehehe")
+bc.create_block("hehehe")
 print(bc.chain[0]["Block"])
 print(bc.chain[1]["Block"])
 print(bc.block_owners)
