50ce121cb267300eaeb62b50b03a3d2b2c946d2c
Patryk Janowski <pkjanowski@gmail.com>
2023-10-15 13:47:58 +0200

    _

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from hashlib import sha256
-import json
-import os, struct
-import socket
+import json, secrets, base64, os, struct, socket
 from jsonpath import JSONPath
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.exceptions import InvalidSignature
+
+
 
 class Block:
-    def __init__(self, index, previous_hash, data, owner, nonce=0):
-        self.owner = owner
+    def __init__(self, index, previous_hash, data, owner_key, nonce=0):
+        self.owner_key = owner_key
         self.index = index
         self.previous_hash = previous_hash
         self.data = data
@@ -26,7 +29,7 @@ class Block:
         return self.hash.startswith('1' * 4)
     
     def __str__(self):
-        return f"""Owner: {self.owner}
+        return f"""Owner: {self.owner_key}
 Index: {self.index}
 Previous hash: {self.previous_hash}
 Data: {self.data}
@@ -39,17 +42,21 @@ Hash: {self.hash}"""
     
 class Blockchain():
     def __init__(self, blockchain_address):
+        self.my_id = self.generate_secure_id()
         # self.users_ips = self.join_blockchain(blockchain_address)
         self.chain = list()
-        self.my_id = struct.unpack('I', os.urandom(4))[0]
         self.my_ip = self.get_my_ip()
+        self.pub_key, self.__priv_key = self.generate_keys(self.my_id)
+        self.users= dict()
         self.owners_ip = dict()
 
+
     def join_blockchain(self, blockchain_address):
-        self.send_ips()
-        self.listen_for_ips()
+        self.send_user_info()
+        self.listen_for_users()
         self.listen_for_blocks()
         pass
+
 
     def publish_block(self, block: Block, block_hash):
         #port 50000
@@ -61,29 +68,36 @@ class Blockchain():
             self.chain = tmp_blockchain
         return True
     
+
     def send_blockchain(self):
         pass
+
 
     def listen_for_blocks(self):
         #port 50000
         pass
 
+
     def wait_for_votes(self):
         #port 50001
         pass
+
 
     def vote_for_block(self):
         #port 50001
         pass
 
-    def send_ips(self):
+
+    def send_user_info(self):
         #port 50002
         pass
 
-    def listen_for_ips(self):
+
+    def listen_for_users(self):
         #port 50002
         self.send_blockchain()
         pass
+
 
     def get_my_ip(self):
         try:
@@ -96,22 +110,90 @@ class Blockchain():
             print(f"Unable to get Hostname and IP. Error: {str(err)}")
 
 
+    def generate_keys(self, prefix):
+        # Generate private key
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+        )
+        # Generate public key
+        public_key = private_key.public_key()
+        # Save private key
+        with open(f"{prefix}_private_key.pem", "wb") as f:
+            f.write(private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption(),
+            ))
+
+        # Save public key
+        with open(f"{prefix}_public_key.pem", "wb") as f:
+            f.write(public_key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo,
+            ))
+
+        return public_key, private_key
+    
+
+    def sign_message(self, message):
+        signature = self.__priv_key.sign(
+            message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return signature
+    
+
+    def verify_signature(self, public_key_pem, message, signature):
+        public_key = serialization.load_pem_public_key(public_key_pem)
+        try:
+            public_key.verify(
+                signature,
+                message,
+                padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH
+                ),
+                hashes.SHA256()
+            )
+            return True
+        except InvalidSignature:
+            return False
+        
+
+
+    def generate_secure_id(self, length=32):
+        # Generate random bytes
+        random_bytes = secrets.token_bytes(length)
+        
+        # Encode the bytes in URL-safe base64 format
+        secure_id = base64.urlsafe_b64encode(random_bytes).rstrip(b'=')
+        
+        return secure_id.decode('utf-8')
+
+
+
+
     @property
     def block_owners(self):
-        return set(x["Block"].owner for x in self.chain)
+        return set(x["Block"].owner_key for x in self.chain)
     
     def create_blockchain(self, data):
         #create initial block
-        new_block = Block(owner=self.my_id, 
+        new_block = Block(owner_key=self.pub_key, 
                           data=data, 
                           index=len(self.chain),
                           previous_hash="0")
         new_block_hash = new_block.mine_block()
         self.chain.append({"Block": new_block, "BlockHash": new_block_hash})
-        self.owners_ip[self.my_id] = self.my_ip 
+        self.owners_ip[self.pub_key.public_numbers()] = self.my_ip 
         
     def create_block(self, data):
-        new_block = Block(owner=self.my_id, 
+        new_block = Block(owner_key=self.pub_key, 
                           data=data, 
                           index=len(self.chain),
                           previous_hash=self.chain[-1]["BlockHash"])
